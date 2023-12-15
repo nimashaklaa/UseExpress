5286cbdebc208c8c177563fad24dab7d43dd5802
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2023-12-15 23:56:02 +0530

    usestate

---
+++ hello.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+/* use allow us to add a new middleware function
+
+const server = http.createServer(app);
+
+server.listen(3000);
